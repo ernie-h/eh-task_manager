e210e4e310bbc673fac983807f5bc5bf17eff0f7
Ernie Hao <ernie.hao@americastestkitchen.com>
2018-04-13 16:01:56 -0400

    Eslint config files added

--- App.js
+++ App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import React from 'react'
+import { StyleSheet, ScrollView, View, Image, Text } from 'react-native'
 
 export default class App extends React.Component {
-  render() {
+  render () {
     return (
       <ScrollView style={styles.scrollView}>
-
-      <View style={styles.imgContainer}>
-        <Image style={styles.image} source={{uri: 'http://www.reactnativeexpress.com/logo.png'}}/>
-      </View>
-        
-      <View style={styles.textContainer}>
-        <Text style={styles.text}> Hello World</Text>
-        <Text style={styles.text}> ma name ernie</Text>
-        <Text style={styles.text}> shakkabrah!</Text>
-        <View style={styles.box}/>
-      </View>
-     
+        <View style={styles.imgContainer}>
+          <Image style={styles.image} source={{uri: 'http://www.reactnativeexpress.com/logo.png'}}/>
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}> Hello World</Text>
+          <Text style={styles.text}> ma name ernie</Text>
+          <Text style={styles.text}> shakkabrah!</Text>
+          <View style={styles.box}/>
+        </View>
       </ScrollView>
-    );
+    )
   }
 }
 
@@ -27,40 +24,38 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-    textContainer: {
+  textContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'flex-start',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 
   scrollView: {
     flex: 1
   },
-    box: {
-    justifyContent: "flex-end",
+  box: {
+    justifyContent: 'flex-end',
     width: 400,
     height: 200,
     backgroundColor: 'white',
     borderWidth: 4,
     borderColor: 'gray',
-    borderRadius: 20,
-    //position: 'absolute'
+    borderRadius: 20
   },
-    text: {
+  text: {
     backgroundColor: 'whitesmoke',
-    justifyContent: "flex-end",
+    justifyContent: 'flex-end',
     color: '#4A90E2',
     fontSize: 24,
-    padding: 10,
+    padding: 10
 
-    },
+  },
 
-    image: {
+  image: {
     width: 200,
     height: 200
-    //position: "left",
-  },
-});
+  }
+})
