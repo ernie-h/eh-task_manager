4287cf6ffa3ff71fbd9e9494ab780f696acb19e6
Ernie Hao <ernie.hao@americastestkitchen.com>
2018-04-23 16:50:43 -0400

    Added components folder/headerLayout

--- App.js
+++ App.js
@@ -2,32 +2,16 @@
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
 
-import styled from 'styled-components';
+import HeaderLayout from './Components/HeaderLayout';
 
-const Header = styled.Text`
-  padding: 0.5px;
-  margin: 0.5px;
-  color: palevioletred;
-  background: papayawhip;
-  border-radius: 10px;
-  fontSize: 30px;
-
-`;
-
-const HeaderWrapper = styled.View`
-  alignSelf: center;
-  padding-top: 50px;
-  height: 100px;
-`;
-
-export default class HomeLayout extends Component {
+class HomeLayout extends Component {
   render() {
     return (
       <ScrollView>
-        <HeaderWrapper>
-          <Header> Ernie's Task Manager </Header>
-        </HeaderWrapper>
+        <HeaderLayout />
       </ScrollView>
     );
   }
 }
+
+export default HomeLayout;
